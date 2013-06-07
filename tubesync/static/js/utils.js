fb8f217703a2cd0d36d1023b7e5eacdba4d8e30b
Andrew <forkk@forkk.net>
2013-06-06 19:17:30 -0500

    Separated JavaScript and CSS from HTML.
    Also, merged ajaxerrors.js into utils.js and included utils.js in the base template.

--- tubesync/static/js/utils.js
+++ tubesync/static/js/utils.js
@@ -54,3 +54,47 @@ function getTimeStr(timeSecs, forceHrs)
 	time += minutes + ":" + seconds;
 	return time;
 }
+
+// Returns a human readable error message for the given jQuery error.
+function getJQErrorMsg(jqError)
+{
+	switch (jqError)
+	{
+	case "timeout":
+		return "The operation timed out.";
+
+	case "abort":
+		return "The operation was aborted.";
+
+	case "parsererror":
+		return "The server returned an invalid response.";
+
+	default:
+		return "An unknown error occurred.";
+	}
+}
+
+// Returns a human readable error message for the given HTTP status code.
+function getHTTPErrorMsg(httpCode)
+{
+	switch (httpCode)
+	{
+	case 400:
+		return "Status code: " + httpCode + " - The server got a bad request. This is likely an error with the application.";
+
+	case 400:
+		return "Status code: " + httpCode + " - The requested page was not found. This is likely an error with the application.";
+
+	case 500:
+		return "Status code: " + httpCode + " - The server encountered an error processing the request. Try again later.";
+
+	case 502:
+		return "Status code: " + httpCode + " - Bad gateway. The server is probably down. Try again later.";
+
+	case 504:
+		return "Status code: " + httpCode + " - Gateway timeout. The server timed out. Try again later.";
+
+	default:
+		return "Got an unknown HTTP error " + httpCode + ".";
+	}
+}

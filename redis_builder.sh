ecb0a2513f3bba5d1748b92443864715b62c43a6
Andrew <forkk@forkk.net>
2013-06-27 12:18:54 -0500

    Only compile redis when necessary.

--- redis_builder.sh
+++ redis_builder.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-echo "Downloading redis"
-wget http://redis.googlecode.com/files/redis-2.6.14.tar.gz
-tar xzf redis-2.6.14.tar.gz
+
+[ -d "redis-2.6.14" ] ||
+{
+	echo "Downloading redis";
+	wget http://redis.googlecode.com/files/redis-2.6.14.tar.gz;
+	tar xzf redis-2.6.14.tar.gz;
+}
+
 cd redis-2.6.14
+
 echo "Building redis"
 make
 cp src/redis-server ~/redis-server

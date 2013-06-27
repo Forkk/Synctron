3b5b970a2fa2f04a5cc5eb0e53916fc33f8740e9
Andrew <forkk@forkk.net>
2013-06-27 13:37:00 -0500

    Fixed redis_builder script again...

--- redis_builder.sh
+++ redis_builder.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
 
-[ -d "redis-2.6.14" ] ||
+REDIS_VERSION="2.6.14"
+
+# Save the build dir path to a variable.
+BUILD_DIR=$PWD
+
+# Run this build in ~ so build files aren't deleted with the temp build folder.
+# This is so we don't have to re-compile redis *every* time changes are pushed (that would be unnecessary).
+cd ~
+
+[ -d "redis-${REDIS_VERSION}" ] ||
 {
 	echo "Downloading redis";
-	wget http://redis.googlecode.com/files/redis-2.6.14.tar.gz;
-	tar xzf redis-2.6.14.tar.gz;
+	wget http://redis.googlecode.com/files/redis-${REDIS_VERSION}.tar.gz;
+	tar xzf redis-${REDIS_VERSION}.tar.gz;
 }
 
-cd redis-2.6.14
+cd redis-${REDIS_VERSION}
 
 echo "Building redis"
 make
+make test
 cp src/redis-server ~/redis-server
-cp ../redis.conf ~/redis.conf.in1
+
+cd ${BUILD_DIR}
+cp redis.conf ~/redis.conf.in1
